eeaeb404f0a5dc484f87487de9cb2cdc924043c6
lax <ax_sunshine@163.com>
2026-03-16 18:17:05 +0800

    fix

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,2 +1,25 @@
 // todo 工具类方法
 
+// ? 测试
+export default {
+  install(Vue, options) {
+    console.log('install')
+    console.log(arguments)
+    Vue.sayHello = function () {
+      console.log('hello world!')
+    }
+    Vue.prototype.sayHi = function () {
+      console.log('Hi')
+    }
+    Vue.directive('throttle', function (el, binding) {
+      let timer = null
+      el.addEventListener(binding.value.type, function () {
+        if (timer) return
+        binding.value.cb()
+        timer = setTimeout(() => {
+          timer = null
+        }, binding.value.wait)
+      })
+    })
+  }
+}
